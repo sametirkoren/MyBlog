a43777985cf59bb5c1e60fe3c9cb658d881f52f0
Samet İrkören <sametirkoren@gmail.com>
2020-06-14 16:14:31 +0300

    Generic Interface

---
+++ MyBlog.DataAccess/Interfaces/IGenericDal.cs
@@ -0,0 +1,19 @@
+﻿using MyBlog.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.DataAccess.Interfaces
+{
+    public interface IGenericDal<TEntity> where TEntity: class , ITable , new()
+    {
+        Task<List<TEntity>> GetAllAsync();
+        Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity,bool>> filter);
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> filter);
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task RemoveAsync(TEntity entity);
+    }
+}
