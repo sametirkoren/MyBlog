dd0d69fba3767654765eaff5df788a1d4f763587
Samet İrkören <sametirkoren@gmail.com>
2020-06-14 17:56:12 +0300

    IoC Container extend

---
+++ MyBlog.Business/Containers/MicrosoftIoC/CustomIoCExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MyBlog.Business.Concrete;
+using MyBlog.Business.Interfaces;
+using MyBlog.DataAccess.Concrete.EntityFrameworkCore.Repositories;
+using MyBlog.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBlog.Business.Containers.MicrosoftIoC
+{
+    public static class CustomIoCExtension
+    {
+        public static void AddDependencies(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericDal<>), typeof(EfGenericRepository<>));
+            services.AddScoped(typeof(IGenericService<>), typeof(GenericManager<>));
+        }
+    }
+}
