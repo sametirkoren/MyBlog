5917abe278dfa3179aa73e7d4395ce7bbb2d93c0
Samet İrkören <sametirkoren@gmail.com>
2020-06-14 19:14:49 +0300

    Fluent Api CategoryBlogMap

--- MyBlog.DataAccess/Concrete/EntityFrameworkCore/Mapping/CategoryBlogMap.cs
+++ MyBlog.DataAccess/Concrete/EntityFrameworkCore/Mapping/CategoryBlogMap.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyBlog.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MyBlog.DataAccess.Concrete.EntityFrameworkCore.Mapping
 {
-    class CategoryBlogMap
+    public class CategoryBlogMap : IEntityTypeConfiguration<CategoryBlog>
     {
+        public void Configure(EntityTypeBuilder<CategoryBlog> builder)
+        {
+            builder.HasKey(I => I.Id);
+            builder.Property(I => I.Id).UseIdentityColumn();
+
+            builder.HasIndex(I => new { I.BlogId, I.CategoryId }).IsUnique();
+        }
     }
 }
