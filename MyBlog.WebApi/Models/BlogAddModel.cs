bf5b711516b37014bdee00a5b46a442e88f51b43
Samet İrkören <sametirkoren@gmail.com>
2020-06-16 21:13:47 +0300

    WebApi BlogAddModel

---
+++ MyBlog.WebApi/Models/BlogAddModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlog.WebApi.Models
+{
+    public class BlogAddModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public IFormFile Image { get; set; }
+    }
+}
