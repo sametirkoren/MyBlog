1a388511a1ce42b73f45fd2f9cc853e2e1dda858
Samet İrkören <sametirkoren@gmail.com>
2020-06-16 20:51:23 +0300

    WebApi Blog GetAll

--- MyBlog.Business/Containers/MicrosoftIoC/CustomIoCExtension.cs
+++ MyBlog.Business/Containers/MicrosoftIoC/CustomIoCExtension.cs
@@ -15,6 +15,9 @@ namespace MyBlog.Business.Containers.MicrosoftIoC
         {
             services.AddScoped(typeof(IGenericDal<>), typeof(EfGenericRepository<>));
             services.AddScoped(typeof(IGenericService<>), typeof(GenericManager<>));
+
+            services.AddScoped<IBlogService, BlogManager>();
+            services.AddScoped<IBlogDal, EfBlogRepository>();
         }
     }
 }
---
+++ MyBlog.WebApi/Controllers/BlogsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyBlog.Business.Interfaces;
+
+namespace MyBlog.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        private readonly IBlogService _blogService;
+        public BlogsController(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _blogService.GetAllSortedByPostedTimeAsync());
+        }
+    }
+}
